5376da7e0d55fd2224e660e16e59562ae1e22dc2
tim <tim.l@decode-group.com>
2026-05-26 11:57:47 +0800

    revert to old code

--- ctx.go
+++ ctx.go
@@ -1,9 +1,14 @@
 package bifu_payment_sdk
 
-import "context"
+import (
+	"context"
 
-var callbackURLCtxKey struct{ string } = struct{ string }{"callbackURL"}
+	"github.com/decodeex/bifu-payment-sdk/internal/ctxutil"
+)
 
+// InjectCallbackURL 注入回调地址
+// 一些时候, 需要一些独立于全局的配置. 这些配置散落在各个请求中又比较啰唆, 因此通过ctx做定制
+// 并不是所有渠道都支持
 func InjectCallbackURL(ctx context.Context, callbackURL string) context.Context {
-	return context.WithValue(ctx, callbackURLCtxKey, callbackURL)
+	return ctxutil.InjectCallbackURL(ctx, callbackURL)
 }
